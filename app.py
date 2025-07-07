21d7f376a6ddab437320a175d6de18e16666632f
Jinxiong You <jinxiongyou@outlook.com>
2025-07-07 12:38:01 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1617,5 +1617,5 @@ def update_chart_titles(immigrant_status, selected_country):
 
 # === Run App ===
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8050, debug=True)
+    app.run(host='0.0.0.0', port=8050)
 
